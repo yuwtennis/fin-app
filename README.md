c48469b749a15671b237cee986f4270aaa061fef
ywatanabe <yu.w.tennis@gmail.com>
2025-11-24 13:54:32 +0900

    feat: Dashboard tutorial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # fin-app
+
+This repository is intended for practising [financial app for NextJS](https://nextjs.org/learn/dashboard-app/getting-started).
+
+## Next.js App Router Course - Starter
+
+This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
+
+For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '@/app/ui/global.css';
+import {inter} from "@/app/ui/fonts";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard'
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
